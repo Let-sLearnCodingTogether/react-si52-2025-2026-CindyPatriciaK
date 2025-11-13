436392e1d925344bd854067dd3a7da2c9cf4c199
CindyPatriciaK <cindypatriciakadir_2327240008@mhs.mdp.ac.id>
2025-11-13 16:11:16 +0700

    Warna

--- src/pages/movies/Movies.tsx
+++ src/pages/movies/Movies.tsx
@@ -2,8 +2,8 @@ import { NavLink } from "react-router"
 
 function Movies (){
     return <div>
-        <h2> Movie Page</h2>
-        <NavLink to="/add-movie" className="btn btn-primary">Add Movie</NavLink>
+        <h2 style={{ color: "#662222" }}> Movie Page</h2>
+        <NavLink to="/add-movie"style={{ backgroundColor: "#EE6983", borderColor: "#ff0a54" }} className="btn btn-primary">Add Movie</NavLink>
     </div>
 }
 export default Movies
